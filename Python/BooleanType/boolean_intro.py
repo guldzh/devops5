864308483e1c205a83c8ebb3e1720b4726b92aed
nurzhigitgulaiym <gdzhorobekova@gmail.com>
2023-09-25 19:07:08 -0500

    Boolean notes added

---
+++ Python/BooleanType/boolean_intro.py
@@ -0,0 +1,20 @@
+#Creating boolean variable
+is_Raining = False 
+print(is_Raining)
+
+#Lets look at the type of the variable
+print(type(is_Raining))
+
+is_September = True
+print(is_September) 
+print(type(is_September))
+
+# Code below will generate an error, is_September = true. 
+# True is not same as true. True is python reserved kewyword 
+
+a=0
+false=a
+new=false
+print(new)
+
+print(5.0 >0)
